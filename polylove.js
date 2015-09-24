1c92561edba0cb6028b9c45b551fa73c725d4994
jaune <JauneLaCouleur@gmail.com>
2015-09-24 07:47:19 +0200

    Fix OrderBy parameters

--- polylove.js
+++ polylove.js
@@ -1,195 +1,13 @@
 var lf = require('lovefield');
-var sqlSelectParse = require('polylove-sql-parser');
 var lfDatabase;
 
-var lfReadyCallbacks = [];
-function lfReady(cb) {
-  lfReadyCallbacks.push(cb);
-}
+var lfBehaviorBuilder = require('./lib/BehaviorBuilder.js');
+var QueryManager = require('./lib/QueryManager.js');
 
-function lfQueryBuilder (db, options) {
-  this.db = db;
-  this.alias = {};
-  this.options = options;
-
-  this.propertyBindingIndexies = {};
-  this.constants = options.constants;
-}
-
-lfQueryBuilder.prototype.table = function(n) {
-  var t;
-  if(typeof n == 'string') {
-    if (!this.alias[n]) { 
-      this.alias[n] = this.db.getSchema().table(n);
-    }
-    return this.alias[n];
-  }
-
-  if (Array.isArray(n)) {
-    if (!this.alias[n[1]]) {
-      this.alias[n[1]] = this.db.getSchema().table(n[0]);
-    }
-    return this.alias[n[1]];
-  }
-
-  throw 'Invalid table name';      
-};
-
-lfQueryBuilder.prototype.build = function() {
-  var q, db = this.db;
-
-  var queryObject = this.options.query;
-
-  var tables = {};
-
-  // SELECT
-  if (queryObject.columns.length === 1 && queryObject.columns[0] === '*'){
-    q = db.select();
-  } else {
-    throw 'TODO';
-  }
-
-  // FROM
-  var fromArguments = []
-  queryObject.from.forEach(function (from) {
-    fromArguments.push(this.table(from))
-  }, this);
-  q.from.apply(q, fromArguments);
-
-  // WHERE
-  if (Array.isArray(queryObject.where)) {
-    q.where(this.buildWhere(queryObject.where));
-  }
-
-   
-
-  // ORDER BY
-  if (Array.isArray(queryObject.orderBy)) {
-    queryObject.orderBy.forEach(function (o) {
-      var d;
-
-
-
-      if (typeof o[2] == 'string') {
-        d = lf.Order[o[2].toUpperCase()];
-      } else if (typeof o[2] == 'object') {
-        switch (o[2].type) {
-          case 'constant':
-            d = lf.Order[this.constants[o[2].name].toUpperCase()];
-          break;
-          case 'property':
-            console.log('----*----');
-            d = this.bind(o[2]);
-          break;
-          default:
-            throw 'Unsupported binding direction !'
-        }
-      }
-      else {
-        throw 'Invalid direction !'
-      }
-      q.orderBy(this.table(o[0])[o[1]], d);
-    }, this);
-  }
-  return q;
-};
-
-lfQueryBuilder.prototype.bind = function (value) {
-  if (value.type == 'property') {
-    if (!this.propertyBindingIndexies[value.name]) {
-      this.propertyBindingIndexies[value.name] = Object.keys(this.propertyBindingIndexies).length;
-    }
-    return this.propertyBindingIndexies[value.name];
-    
-  }
-  throw 'TODO';
-};
-
-lfQueryBuilder.prototype.buildWhere = function (w) {
-  var me = this;
-
-  switch(w[0]) {
-    // A = B
-    case '=': return (function () {
-      var a, b;
-
-      a = me.table(w[1][0])[w[1][1]];
-      // ['=', ['table', 'a'], {type: 'property', name: 'b', index: 0}]
-      if (w[2].type === 'property') {
-        b = lf.bind(me.bind(w[2]));
-      // ['=', ['table', 'a'], {type: 'constant', name: 'b', index: 0}]
-      } else if (w[2].type === 'constant') {
-        b = this.constants[w[2].name];
-      } else {
-        throw 'Error';
-      }
-      
-      return a.eq(b);
-    })();
-  }
-};
-
-function lfBuildQuery(db, options) {
-  return (new lfQueryBuilder(db, options)).build();
-}
-
-function lfParseSQLSelect(queryObject, constants) {
-
-  return function (db) { return lfBuildQuery(db, this); }.bind({
-    constants: constants,
-    query: queryObject
-  });
-}
-
-function lfBehaviorBuilder() {
-  this.behavior = {
-    observers: [],
-    properties: {}
-  };
-}
-
-lfBehaviorBuilder.prototype.push = function(propertyName, queryString, constants) {
-  constants = constants || {};
-
-  var query = null;
-  var queue = [];
-  var queryObject = sqlSelectParse(queryString)
-  var createQuery = lfParseSQLSelect(queryObject, constants);
-
-  this.behavior.properties[propertyName] = {
-    type: Array,
-    value: [],
-    notify: true
-  };
-
-  this.behavior.observers.push('_'+propertyName+'Observer('+queryObject.properties.join(', ')+')');
-
-  lfReady(function (db) {
-    query = createQuery(db)
-    var task;
-    while (task = queue.shift()){
-      runQuery.apply(null, task);
-    }
-  });
-
-  function runQuery(el, binds) {
-    if (!query) {
-      queue.push([el, binds]);
-      return;
-    }
-    query.bind(binds).exec().then(function (rows) {
-      el.notifyPath(propertyName, rows);
-    });
-  }
-
-  this.behavior['_'+propertyName+'Observer'] = function () {
-    runQuery(this, Array.prototype.slice(arguments, 0));
-  }; 
-};
-
+var qm = new QueryManager();
 
 function lfBehavior(options) {
-  var builder = new lfBehaviorBuilder();
+  var builder = new lfBehaviorBuilder(qm);
 
   Object.keys(options).forEach(function (propertyName) {
     if (typeof options[propertyName] == 'string') {
@@ -202,44 +20,16 @@ function lfBehavior(options) {
   return builder.behavior;
 }
 
-
-/*
-(function (db) {
-  var t = db.getSchema().table('Movie');
-
-  var rows = [];
-
-  rows.push(t.createRow({
-    'id': 666,
-    'title': 'Diablo',
-    'year': 2014,
-  }));
-  db.insertOrReplace().into(t).values(rows).exec().then(function () {
-    console.log('DONE !');
-  });
-
-})(lfDatabase);
-*/
-
-function lfConnect (schemaBuilder) {
-  return schemaBuilder.connect()
-  .then(function (database) {
-    return lfDatabase = database;
-  })
-}
-
 module.exports = {
   lf: lf,
+  qm: qm,
   ready: function (db) {
-    lfReadyCallbacks.forEach(function (cb) {
-      cb.call(null, db);
-    });
+    qm.setDatabase(db);
   },
-  connect: lfConnect,
   behavior: lfBehavior,
   getErrorMessage: function (error) {
     var error_code = require('json!./node_modules/lovefield/dist/error_code.json');
-    var parts = url.parse(error.message, true);
+    var parts = require('url').parse(error.message, true);
     var message = error_code[error.code];
 
     Object.keys(parts.query).forEach(function (param) {
--- test/polylove.js
+++ test/polylove.js
@@ -1,76 +1,79 @@
 var expect = chai.expect;
 
-
-
 describe('Polylove', function() {
 	
+  before(function(done) {
+    var db;
+
+    // create schema
+    var schemaBuilder = lf.schema.create('tests.mvdb', 1);
+
+    schemaBuilder.createTable('Movie')
+      .addColumn('id', lf.Type.INTEGER)
+      .addColumn('title', lf.Type.STRING)
+      .addColumn('year', lf.Type.INTEGER)
+      .addColumn('rating', lf.Type.STRING)
+      .addColumn('company', lf.Type.STRING)
+      .addPrimaryKey(['id'])
+      .addNullable(['year', 'rating', 'company'])
+    ;
+
+    schemaBuilder.connect()
+    .then(function(database) {
+      db = database;
+
+      var t = db.getSchema().table('Movie');
+
+      var rows = [];
+      
+      rows.push(t.createRow({
+        'id': 1,
+        'title': 'Get a cup of coffee',
+        'year': 2015,
+      }));
+
+      rows.push(t.createRow({
+        'id': 2,
+        'title': 'Qsfq sf qsfqsf',
+        'year': 2015,
+      }));
+
+      rows.push(t.createRow({
+        'id': 3,
+        'title': 'Ezea za dazdad',
+        'year': 2014,
+      }));
+
+      rows.push(t.createRow({
+        'id': 4,
+        'title': 'Dsdg ezegz eg',
+        'year': 2014,
+      }));        
+
+      return db.insertOrReplace().into(t).values(rows).exec();
+    })
+    .then(function () {
+      console.log('---Ready---');
+      polylove.ready(db);
+      done();
+    })
+    .catch(function (error) {
+      if (!error.code) {
+        throw error;
+        return;
+      }
+      throw polylove.getErrorMessage(error);
+    });
+  });
+
 	it('polylove global', function () {
 		expect(polylove).to.be.an('object');	
 	});
-	 
+
+/*
   describe('#behavior()', function() {
   	
-    before(function(done) {
-      // create schema
-      var schemaBuilder = lf.schema.create('tests.mvdb', 1);
 
-      schemaBuilder.createTable('Movie')
-        .addColumn('id', lf.Type.INTEGER)
-        .addColumn('title', lf.Type.STRING)
-        .addColumn('year', lf.Type.INTEGER)
-        .addColumn('rating', lf.Type.STRING)
-        .addColumn('company', lf.Type.STRING)
-        .addPrimaryKey(['id'])
-        .addNullable(['year', 'rating', 'company'])
-      ;
-
-      polylove.connect(schemaBuilder)
-      .then(function(database) {
-        db = database;
-
-        var t = db.getSchema().table('Movie');
-
-        var rows = [];
-        
-        rows.push(t.createRow({
-          'id': 1,
-          'title': 'Get a cup of coffee',
-          'year': 2015,
-        }));
-
-        rows.push(t.createRow({
-          'id': 2,
-          'title': 'Qsfq sf qsfqsf',
-          'year': 2015,
-        }));
-
-        rows.push(t.createRow({
-          'id': 3,
-          'title': 'Ezea za dazdad',
-          'year': 2014,
-        }));
-
-        rows.push(t.createRow({
-          'id': 4,
-          'title': 'Dsdg ezegz eg',
-          'year': 2014,
-        }));        
-
-        return db.insertOrReplace().into(t).values(rows).exec();
-      })
-      .then(function () {
-        console.log('---DONE---');
-        done();
-      })
-      .catch(function (error) {
-        if (!error.code) {
-          throw error;
-          return;
-        }
-        throw polylove.getErrorMessage(error);
-      });
-      
-    });
 
     after(function() {
     });
@@ -78,11 +81,64 @@ describe('Polylove', function() {
     it('SELECT', function () {
       var b = polylove.behavior({
         'movies': 'SELECT * FROM Movie WHERE Movie.year = {{year}} ORDER BY Movie.title {{direction}}'
-      })
+      });
 
       expect(b.observers).to.deep.equal(['_moviesObserver(year, direction)']);
       expect(b._moviesObserver).to.be.a('function');
     });
 
   });
+*/
+  describe('QueryManager', function() {
+    describe('#createQuery()', function() {
+      it('SELECT * FROM Movie WHERE Movie.year = {{year}} ORDER BY Movie.title {{direction}}', function () {
+        var q = polylove.qm.create('SELECT * FROM Movie WHERE Movie.year = {{year}} ORDER BY Movie.title {{direction}}');
+      });
+    });
+  });
+
+  describe('Query', function() {
+    describe('#run()', function() {
+      it('"SELECT * FROM Movie WHERE Movie.year = {{year}}" with { year: 2014 }', function (done) {
+        var q = polylove.qm.create('SELECT * FROM Movie WHERE Movie.year = {{year}}');
+
+        q.run({ year: 2014 }, function (error, rows) {
+          if (error) {
+            if (error.code){
+              return done(polylove.getErrorMessage(error)); 
+            }
+            return done(error);
+          }
+
+          expect(rows).to.deep.equal([
+            { id: 3, title: "Ezea za dazdad", year: 2014},
+            { id: 4, title: "Dsdg ezegz eg", year: 2014}            
+          ]);
+
+          return done();
+        });
+      });
+
+      it('"SELECT * FROM Movie WHERE Movie.year = {{year}} ORDER BY Movie.title :direction" with { year: 2014 } and { direction : "ASC" }', function (done) {
+        var q = polylove.qm.create('SELECT * FROM Movie WHERE Movie.year = {{year}} ORDER BY Movie.title :direction', {direction: 'ASC'});
+
+        q.run({ year: 2014 }, function (error, rows) {
+          if (error) {
+            if (error.code){
+              return done(polylove.getErrorMessage(error)); 
+            }
+            return done(error);
+          }
+
+          expect(rows).to.deep.equal([
+            { id: 4, title: "Dsdg ezegz eg", year: 2014},
+            { id: 3, title: "Ezea za dazdad", year: 2014}
+          ]);
+
+          return done();
+        });
+      });
+    });
+  });
+
 });
